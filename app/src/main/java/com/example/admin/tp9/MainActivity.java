640ae8a141e8b9f5883c39cedc2298c25fc3f71c
Thibaut Terrasse <thibautterrasse@hotmail.fr>
2017-06-13 15:24:56 +0200

    ajout de fonctions pour sauvegarder les données quand on fait une rotation d'écran

--- app/src/main/java/com/example/admin/tp9/MainActivity.java
+++ app/src/main/java/com/example/admin/tp9/MainActivity.java
@@ -6,24 +6,54 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Traceur{
+
+    public static final String PROGRESS = "progress";
+    int index = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button bouton1 = (Button) findViewById(R.id.button1);
+        final Button bouton1 = (Button) findViewById(R.id.button1);
+        final TextView text = (TextView) findViewById(R.id.compteur);
 
         bouton1.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 Log.d("Bouton", "Bouton 1");
-                Intent intent = new Intent(MainActivity.this, Secondaire.class);
-                startActivity(intent);
+                index++;
+                text.setText(index+"");
+
+                //Intent intent = new Intent(MainActivity.this, Secondaire.class);
+                //startActivity(intent);
             }
         });
     }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String progress = savedInstanceState.getString(PROGRESS);
+        index=Integer.parseInt(progress);
+        TextView text = (TextView) findViewById(R.id.compteur);
+
+        text.setText(progress);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle onState) {
+        super.onSaveInstanceState(onState);
+        TextView text = (TextView) findViewById(R.id.compteur);
+
+        String progress = text.getText().toString();
+
+        Log.d("ETAT","text"+progress);
+        onState.putString(PROGRESS,progress);
+
+    }
+
 }
